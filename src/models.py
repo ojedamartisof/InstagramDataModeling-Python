466552a85a7efe05ecf9a9f79c045e1d8b401779
ojedamartisof <ojedamartinasofia@gmail.com>
2020-11-16 17:37:42 +0000

    The project is using the SQLAlchemy Python library to generate the database

--- src/models.py
+++ src/models.py
@@ -8,26 +8,63 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(30), nullable=False)
+    password = Column(String(250), nullable=False)
+    fullname = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=False)
+    phone_num = Column(Integer, nullable=False)
+    profile_pic_url = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'followers'
+    timestamp = Column(String(250), nullable=False)  
+    denayorrejectoption = Column(String(250), nullable=True) 
+    follower_status = Column(String(250), nullable=False)  
+    follower_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    follower = relationship(User)
+    user = relationship(User)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    photo_id = Column(String(250), nullable=False)
+    img_url = Column(String(250), nullable=False)
+    timestamp = Column(String(250), nullable=False)
+    pic_descrition = Column(String(250), nullable=True)
+    location = Column(String(250), nullable=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    photo_id = Column(Integer, nullable=False)
+    comment = Column(String(250), nullable=True)
+    img_url = Column(String(250), nullable=False)
+    timestamp = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Post)
+
+class Like(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    photo_id = Column(Integer, nullable=False)
+    liker_id = Column(Integer, nullable=False)
+    timestamp = Column(String(250), nullable=False)
+    img_url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Post)
 
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
